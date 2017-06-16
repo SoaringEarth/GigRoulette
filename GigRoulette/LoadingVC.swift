e73883bc2472fab655df0dd974e33c8b44e27cd2
Ross Patman <ross.patman@livestyled.net>
2017-06-16 15:42:02 +0100

    Add loadingVC

---
+++ GigRoulette/LoadingVC.swift
@@ -0,0 +1,21 @@
+//
+//  LoadingVC.swift
+//  GigRoulette
+//
+//  Created by Ross Patman on 16/06/2017.
+//  Copyright © 2017 apple. All rights reserved.
+//
+
+import UIKit
+
+class LoadingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    
+
+}
