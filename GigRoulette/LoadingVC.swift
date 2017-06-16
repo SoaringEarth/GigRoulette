9a7086a76974512605da042c2f9474c8c7ee0451
Ross Patman <ross.patman@livestyled.net>
2017-06-16 16:14:24 +0100

    Work on animation

--- GigRoulette/LoadingVC.swift
+++ GigRoulette/LoadingVC.swift
@@ -12,34 +12,52 @@ class LoadingVC: UIViewController {
 
     @IBOutlet weak var icon: UIImageView!
     @IBOutlet weak var purchaseButton: UIButton!
+    var animate: Bool = true
     override func viewDidLoad() {
         super.viewDidLoad()
         purchaseButton.isHidden = true
         
         animateIconOut()
         
-        let timer = Timer.scheduledTimer(withTimeInterval: 5, repeats: false) { (timer) in
-            self.purchaseButton.isHidden = false
+        let timer = Timer.scheduledTimer(withTimeInterval: 5.4, repeats: false) { (timer) in
+            
+            self.animate = false
+            self.stopAnimation()
         }
         
     }
     
     func animateIconIn() {
-        UIView.animate(withDuration: 1.5, delay: 0, options: .curveEaseInOut, animations: {
+        guard animate == true else { return }
+        UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut, animations: {
             self.icon.alpha = 1
+            self.icon.transform = CGAffineTransform.init(translationX: -self.view.frame.width/1.5, y: 0)
         }) { (success) in
             self.animateIconOut()
+            
         }
         
     }
     
     func animateIconOut() {
-        UIView.animate(withDuration: 1.5, delay: 0, options: .curveEaseInOut, animations: {
-            self.icon.alpha = 0.5
+        guard animate == true else { return }
+        UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut, animations: {
+            self.icon.alpha = 0.2
+            self.icon.transform = CGAffineTransform.init(translationX: -self.view.frame.width/1.5, y: 0)
         }) { (success) in
+            self.icon.transform = CGAffineTransform.init(translationX: self.view.frame.width, y: 0)
             self.animateIconIn()
         }
         
+    }
+    
+    func stopAnimation() {
+        UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut, animations: {
+            self.icon.alpha = 1.0
+            self.icon.transform = CGAffineTransform.identity
+        }) { (success) in
+            self.purchaseButton.isHidden = false
+        }
     }
     @IBAction func open(_ sender: Any) {
         let chosenVC = ChosenEventViewController(nibName: "ChosenEventViewController", bundle: nil)
