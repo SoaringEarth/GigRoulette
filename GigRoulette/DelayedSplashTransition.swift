7e3f2160f32c059b8893166ac41fb10e1c4e7fdb
J.Albert <jonathon.albert@live.com>
2017-06-19 14:02:22 +0100

    tweaked animation speed

--- GigRoulette/DelayedSplashTransition.swift
+++ GigRoulette/DelayedSplashTransition.swift
@@ -30,7 +30,7 @@ class DelayedSplashTransition: NSObject, UIViewControllerAnimatedTransitioning {
     }
     
     func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
-        return 2.0
+        return 0.35
     }
     
 }
