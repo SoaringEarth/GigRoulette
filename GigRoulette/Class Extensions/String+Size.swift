0a2a24d664cf113031194f2e828e6f08b201684c
J.Albert <jonathon.albert@live.com>
2017-07-17 15:51:10 +0100

    remove unused string extension

--- GigRoulette/Class Extensions/String+Size.swift
+++ GigRoulette/Class Extensions/String+Size.swift
@@ -10,25 +10,6 @@ import UIKit
 import Foundation
 
 extension String {
-    func height(font: UIFont) -> CGFloat {
-        let constraintRect = CGSize(width: 1.0, height: .greatestFiniteMagnitude)
-        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [.font: font], context: nil)
-        
-        return boundingBox.height
-    }
-    
-    func width(font: UIFont) -> CGFloat {
-        let constraintRect = CGSize(width: .greatestFiniteMagnitude, height: 1.0)
-        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [.font: font], context: nil)
-        
-        return boundingBox.width
-    }
-    
-    
-}
-
-
-extension String {
     
     func widthOfString(usingFont font: UIFont) -> CGFloat {
         let fontAttributes = [NSAttributedStringKey.font: font]
