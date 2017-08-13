8eccc7261bb82936e700e94507f4224afb047893
J.Albert <jonathon.albert@live.com>
2017-08-13 13:50:17 +0100

    add webview
    webview displays the current chosen events url for purchasing tickets

--- GigRoulette/ViewControllers/Chosen Event/ChosenEventViewController.swift
+++ GigRoulette/ViewControllers/Chosen Event/ChosenEventViewController.swift
@@ -15,6 +15,8 @@ class ChosenEventViewController: UIViewController {
     
     var chosenEvent: EventEntity?
     
+    var ticketView = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -27,10 +29,36 @@ class ChosenEventViewController: UIViewController {
         imGoing()
     }
     
+    @objc func closeWebView() {
+        ticketView.removeFromSuperview()
+    }
+    
     private func imGoing() {
-        let alertViewController = UIAlertController(title: "Purchase Ticket?", message: "This will lead to the purchasing flow when implemented.", preferredStyle: .alert)
-        alertViewController.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
-        present(alertViewController, animated: true, completion: nil)
+        ticketView = UIView(frame: view.bounds)
+        
+        let closeView = UIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 64))
+        closeView.backgroundColor = UIColor.blue
+        
+        let closeButton = UIButton(frame: CGRect(x: view.bounds.width - 44, y: 20, width: 44, height: 44))
+        closeButton.addTarget(self, action: #selector(ChosenEventViewController.closeWebView), for: .touchUpInside)
+        closeButton.setTitle("X", for: .normal)
+        closeButton.titleLabel?.textColor = UIColor.white
+        closeButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 24.0)
+        closeButton.titleLabel?.textAlignment = .center
+        closeView.addSubview(closeButton)
+        
+        ticketView.addSubview(closeView)
+        
+        let ticketRequest = URLRequest(url: URL(string: chosenEvent!.url)!)
+        let ticketWebView = UIWebView(frame: CGRect(x: 0, y: 64, width: view.bounds.width, height: view.bounds.height))
+        ticketWebView.loadRequest(ticketRequest)
+        ticketView.addSubview(ticketWebView)
+        
+        view.addSubview(ticketView)
+        
+//        let alertViewController = UIAlertController(title: "Purchase Ticket?", message: "This will lead to the purchasing flow when implemented.", preferredStyle: .alert)
+//        alertViewController.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+//        present(alertViewController, animated: true, completion: nil)
         
 //        if let currentLocation = LocationTracker.sharedInstance.currentLocation {
 //            let latString = String(describing: currentLocation.coordinate.latitude)
